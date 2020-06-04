c38e9545648136017a642958dafbd796807dbe88
thealekid <chukorakwusi@gmail.com>
2020-06-04 12:11:24 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Adao", "Charlie", "Ioan"];
+const celebration = ["Christmas"];
+
+function writeCards(names, celebration) {
+    let Messages  = []
+    for (let i = 0; i < names.length; i++) {
+    Messages.push (`Thank you, ${names[i]}, for the wonderful ${celebration} gift!`);
+}
+return Messages
+}
+
+function countDown(number) {
+ while (number > 0) {
+     console.log(number);
+     number -= 1;
+ }
+console.log(number);
+}
